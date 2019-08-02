0acc486fc396ee5fe3558b57cdd8122dedbfa65d
Mike the Knight <mikestevenroberts@gmail.com>
2019-08-02 13:24:43 +0100

    Week 3 wip

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -10,3 +11,11 @@ def index():
             {'author': {'username':'Lainers'}, 'body': 'Jumpin an hollerin'}
     ]
     return render_template('index.html', title='home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember me = {form.remember_me.data}')
+        return redirect('/index')
+    return render_template('login.html', title='Sign in', form=form)
