1932020142d4a17a2035edaa1bfc4b66334d31f2
Mike the Knight <mikestevenroberts@gmail.com>
2019-08-18 19:16:37 +0100

    Chapter 5: Login functionality

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,38 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Mikey boi'}
     posts = [
-            {'author': {'username':'Mikey boi'}, 'body': 'Feeling fly'},
-            {'author': {'username':'Lainers'}, 'body': 'Jumpin an hollerin'}
+        {'author': {'username': 'mikeyboi'}, 'body': "Heyooo!"},
+        {'author': {'username': 'lainey'}, 'body': "Jumpin' and hollerin'"}
     ]
-    return render_template('index.html', title='home', user=user, posts=posts)
+    return render_template('index.html', title='home page', posts=posts)
 
 @app.route('/login', methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'Login requested for user {form.username.data}, remember me = {form.remember_me.data}')
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign in', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
