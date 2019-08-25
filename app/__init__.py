c9a350bbdbd25b34aedf6077b4049e910dad72b1
pikeamus <32355704+pikeamus@users.noreply.github.com>
2019-08-25 16:46:54 +0100

    Update init with file handler formtter

--- app/__init__.py
+++ app/__init__.py
@@ -36,6 +36,8 @@ if not app.debug:
         os.mkdir('logs')
 
     file_handler = RotatingFileHandler('logs/microblog.log', maxbytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
 
